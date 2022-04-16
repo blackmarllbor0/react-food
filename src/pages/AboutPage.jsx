36d6311ab83b674a3acf57097bd674feea5d63d8
blackmarllboro <3100194@gmail.com>
2022-04-16 20:15:51 +0300

    add last routing

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,5 +1,10 @@
 export default function AboutPage() {
     return (
-        <h1>Hello from about page</h1>
+        <h3 style={{
+            lineHeight: 1.4,
+            fontFamily: 'arial'
+        }}>
+            My name is Danya - nickname black marlboro. I am a novice developer from Russia, Moscow.  I am 19 years old and a third-year college student.  I am engaged in web development, I write in React js, but I do not plan to stop there.  Every day is practice and new knowledge, and there is no end to knowledge, so my life is provided with something interesting for a hundred years to come!
+        </h3>
     );
 }
--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -1,5 +1,13 @@
+/* eslint-disable jsx-a11y/iframe-has-title */
 export default function ContactPage() {
     return (
-        <h1>Hello from contact page</h1>
+        <>
+            <h1>My project</h1>
+            <h2><a href="https://github.com/blackmarllbor0" target={'_blank'} rel="noreferrer">Link to my Github</a></h2>
+            <div className="iframe" style={{margin: '5rem 0 0 0'}}>
+                <iframe src="https://blackmarllbor0.github.io/react-movies/" />
+                <iframe src="https://blackmarllbor0.github.io/react-shop/" />
+            </div>
+        </>
     );
 }
